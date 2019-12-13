fce234ade27fd94ed52eec5d312b6221224ccdc6
Avaer Kazmer <a@modules.io>
2019-12-13 17:42:27 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export FLAGS="-I./src/ -I./eigen-eigen-5a0156e40feb/ -I./opencv/ -I./opencv/modules/core/include -I./opencv/build_wasm/ -I./opencv/build/modules/core/ -I./opencv/modules/core/src/ -I./opencv/3rdparty/include/opencl/1.2/ -I./DBoW2/include -I./yaml-cpp/include/ -I./3rd/json/include/ -I./opencv/modules/imgcodecs/include/ -I./opencv/modules/imgproc/include/ -I./opencv/modules/videoio/include/ -I./3rd/spdlog/include/ -I./3rd/popl/include/ -I./g2o -I./g2o/build -I./opencv/modules/features2d/include/ -I./opencv/modules/flann/include/ -I./opencv/modules/calib3d/include/ -I./g2o/EXTERNAL/csparse/ -DUSE_DBOW2=1";
+export FLAGS="-I./src/ -I./eigen-eigen-5a0156e40feb/ -I./opencv/ -I./opencv/modules/core/include -I./opencv/build_wasm/ -I./opencv/build/modules/core/ -I./opencv/modules/core/src/ -I./opencv/3rdparty/include/opencl/1.2/ -I./DBoW2/include -I./yaml-cpp/include/ -I./3rd/json/include/ -I./opencv/modules/imgcodecs/include/ -I./opencv/modules/imgproc/include/ -I./opencv/modules/videoio/include/ -I./3rd/spdlog/include/ -I./3rd/popl/include/ -I./g2o -I./g2o/build -I./opencv/modules/features2d/include/ -I./opencv/modules/flann/include/ -I./opencv/modules/calib3d/include/ -I./g2o/EXTERNAL/csparse/ -DUSE_DBOW2=1 -DSPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_OFF";
 
 # cmake -DMODULE_NAME=core -DOUTPUT=opencl_kernels_core.cpp -DOUTPUT_HPP=opencl_kernels_core.hpp -DCL_DIR=modules/core/src/opencl/ -P ./cmake/cl2cpp.cmake
 # emcc $FLAGS -std=c++11 -o opencv.o -D__OPENCV_BUILD=1 opencv-3.4.0/modules/core/src/*.cpp
@@ -13,4 +13,4 @@ emcc $FLAGS -std=c++11 -O3 -s FILESYSTEM=0 -o yaml.o yaml-cpp/src/*.cpp yaml-cpp
 emcc $FLAGS -O3 -s FILESYSTEM=0 -o csparse.o g2o/EXTERNAL/csparse/*.c;
 emcc $FLAGS -std=c++11 -O3 -s FILESYSTEM=0 -o g2o.o g2o/g2o/core/*.cpp g2o/g2o/stuff/*.cpp g2o/g2o/solvers/csparse/*.cpp
 
-emcc $FLAGS -std=c++11 -O3 -s FILESYSTEM=0 -o run_web.js csparse.o dbow.o openvslam.o yaml.o g2o.o opencv/build_wasm/lib/*.a opencv/build_wasm/3rdparty/lib/libzlib.a ./example/run_web.cc ./example/run_calibration.cc ./example/run_malloc.cc
+emcc $FLAGS -std=c++11 -O3 -s FILESYSTEM=0 -s TOTAL_MEMORY=100MB -o run_web.js csparse.o dbow.o openvslam.o yaml.o g2o.o opencv/build_wasm/lib/*.a opencv/build_wasm/3rdparty/lib/libzlib.a ./example/run_web.cc ./example/run_calibration.cc ./example/run_malloc.cc
